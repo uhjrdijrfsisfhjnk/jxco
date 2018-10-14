a59a98cfe202ec2d3d1cebefaa6840154b4a9ccd
uhjrdijrfsisfhjnk <43994096+uhjrdijrfsisfhjnk@users.noreply.github.com>
2018-10-14 21:01:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,4 +24,25 @@ message.channel.send(`${args}`); // محطوط # عشان محد يستخدم ا
 }
 });
 
+client.on('ready',async () => {
+  let GUILDID = '497502071218503721';
+  let CHANNELID = '501092028306685973'; 
+  voiceStay(GUILDID, CHANNELID);
+  function voiceStay(guildid, channelid) {
+    if(!guildid) throw new Error('Syntax: voiceStay function requires guildid');
+    if(!channelid) throw new Error('Syntax: voiceStay function requires channelid');
+
+    let guild = client.guilds.get(guildid);
+    let channel = guild.channels.get(channelid);
+
+    if(channel.type === 'voice') {
+      channel.join().catch(e => {
+        console.log(`Failed To Join :: ${e.message}`);
+      });
+    } else {
+      console.log(`Channel Type :: ${channel.type}, It must be Voice.`);
+    }
+  }
+});
+
 client.login(process.env.BOT_TOKEN);
